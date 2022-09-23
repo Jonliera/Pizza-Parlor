b6009e2714fffaaaa2809679babcfa8cb91395d1
Jonathan Liera <jonliera1@gmail.com>
2022-09-23 08:08:35 -0700

    add the constructor function for pizza

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//Business logic
+
+function Pizza (size,salami,pepperoni,ham,sausage){
+  this.size = size;
+  this.salami = salami;
+  this.pepperoni = pepperoni;
+  this.ham = ham;
+  this.sausage = sausage;
+}
